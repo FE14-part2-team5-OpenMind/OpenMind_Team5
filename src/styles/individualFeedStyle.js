08c017043a9c60ffecb3071c7c88c9012030a8c6
이나경 <leena3823@gmail.com>
2025-03-15 20:10:26 +0900

    feat: add toast popup for copy URL

--- src/styles/individualFeedStyle.js
+++ src/styles/individualFeedStyle.js
@@ -121,7 +121,6 @@ export const BodyWrapper = styled.div`
     margin: 16px auto;
     gap: 8px;
 
-
     span {
       color: var(--brown40);
       font: var(--body2-bold);
@@ -142,4 +141,23 @@ export const EmptyIcon = styled.img`
   @media (max-width: 480px) {
     margin-top: 35px;
   }
+`;
+
+export const Toast = styled.div`
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 999;
+  padding: 12px 20px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  background: var(--gray60);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  color: var(--gray10);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
 `;
