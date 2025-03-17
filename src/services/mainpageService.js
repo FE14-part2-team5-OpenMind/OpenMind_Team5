8f1c5ebd2b608d385496f2add97b79fc7cb3e53a
이나경 <leena3823@gmail.com>
2025-03-17 15:35:08 +0900

    refactor: integrate api.js logic into main file

--- src/services/mainpageService.js
+++ src/services/mainpageService.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { ENDPOINTS } from '../api/api';
+
+const BASE_URL = 'https://openmind-api.vercel.app/14-5';
+const ENDPOINTS = {
+  mypage: `${BASE_URL}/subjects/`,
+};
 
 export const sendUserName = async (name) => {
   //메인페이지에서 이름 입력받고 피드 생성하는 api
